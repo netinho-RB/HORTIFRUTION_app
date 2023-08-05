9b1b1d32c3c87cf0c5ef407193abe3b97e7c198a
Nestor Nunes <109300513+netinho-RB@users.noreply.github.com>
2023-08-04 21:15:12 -0300

    alteraçoes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Carrinho from './Carrinho';
-import React from 'react';
-import CadastroVenda from './CadastroVenda';
-
-function App() {
-  return (
-    <div>
-      {/* Outros componentes aqui */}
-      <CadastroVenda />
-    </div>
-  );
-}
-
-export default App;
 
 
 const App = () => {
